ef0a55a4d7a160eee90418ea0bba697b81824d2a
Balazs E. Pataki <pataki@gmail.com>
2022-12-12 11:39:59 +0100

    Add --force-yes
    Somehow this is needed when running under Windows WSL.

--- cedar-mongo/include/install_deps.sh
+++ cedar-mongo/include/install_deps.sh
@@ -6,7 +6,7 @@ printf "\n[-] Installing base OS dependencies...\n\n"
 
 # base
 apt-get update
-apt-get install -y --no-install-recommends ca-certificates openssl numactl wget curl
+apt-get install -y --force-yes --no-install-recommends ca-certificates openssl numactl wget curl
 
 
 # Gosu
