3c15921db57a09443dc780949338756f0a853d67
Balazs E. Pataki <pataki@gmail.com>
2023-10-21 11:34:26 +0200

    Fix replacement script more

--- cedar-infra-keycloak/scripts/docker-entrypoint.sh
+++ cedar-infra-keycloak/scripts/docker-entrypoint.sh
@@ -21,7 +21,7 @@ if [ ! -f ${INIT_DONE_FLAG} ]; then
   echo "Keycloak realm not yet imported!"
 
   echo "Importing realm"
-  sed -i.bak 's/CEDAR_HOST/'"$EDAR_HOST"'/g' /Users/balazs/Documents/work/sztaki/arp/dev/cedar-docker-build/cedar-infra-keycloak/config/keycloak-realm.CEDAR.development.2023-07-05.json
+  sed -i.bak 's/CEDAR_HOST/'"$CEDAR_HOST"'/g' /opt/keycloak/keycloak-realm.CEDAR.development.2023-07-05.json
   /opt/keycloak/bin/kc.sh import --file /opt/keycloak/keycloak-realm.CEDAR.development.2023-07-05.json
   /opt/keycloak/bin/kc.sh --verbose build
 
