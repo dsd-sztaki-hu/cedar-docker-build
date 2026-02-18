73559b194d40734fe2a40ffc4c348851b7c2e642
Balazs E. Pataki <pataki@gmail.com>
2026-02-18 18:22:28 +0100

    Revert "fix missing index.html related errors"
    This reverts commit d234cbdf0d7ea1ff75d8f48dbe8ec4a3129669f8.

--- cedar-frontend-openview/scripts/docker-entrypoint.sh
+++ cedar-frontend-openview/scripts/docker-entrypoint.sh
@@ -3,10 +3,7 @@ set -e
 
 echo "Executing sed"
 
-INDEX_HTML="${CEDAR_FRONTEND_HOME}/cedar-openview-dist/index.html"
-if [ -f "$INDEX_HTML" ]; then
-  sed -i 's/window.cedarDomain = \".*\"/window.cedarDomain = \"'${CEDAR_HOST}'\"/g' "$INDEX_HTML"
-  sed -i 's/content\.metadatacenter\.org\//content\.'${CEDAR_HOST}'\//g' "$INDEX_HTML"
-fi
+sed -i 's/window.cedarDomain = \".*\"/window.cedarDomain = \"'${CEDAR_HOST}'\"/g' index.html
+sed -i 's/content\.metadatacenter\.org\//content\.'${CEDAR_HOST}'\//g' index.html
 
 exec "$@"
